50c110afddc203fae2c9462893f98203918e9403
Aditya <adityaprasad9032@gmail.com>
2020-10-14 20:55:44 -0400

    l

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const http = require("http");
+const app = express();
 
+app.use(express.json());
+
+app.post("/", (req, res) => {
+    console.log(req.body.name);
+});
+
+app.listen(80, () => {
+   console.log("Connected!")
+});
