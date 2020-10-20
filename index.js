fc464a0b43af71f41c89538062a22fb367d6d3a6
Krish Thawani <krishthawani46@gmail.com>
2020-10-19 22:51:53 -0400

    fix and refactor some stuff with google oauth

--- index.js
+++ index.js
@@ -67,34 +67,7 @@ app.get('/', (req, res) => {
                     if (error) {
                         console.log('API Response Error: ', error)
                     } else {
-                        body = JSON.parse(body)
-                        var JSONResponse = '<pre><code>' + JSON.stringify(body, null, 2) + '</code></pre>'
-                        // res.send(`
-                        //     <style>
-                        //         @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');@import url('https://necolas.github.io/normalize.css/8.0.1/normalize.css');html {color: #232333;font-family: 'Open Sans', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}h2 {font-weight: 700;font-size: 24px;}h4 {font-weight: 600;font-size: 14px;}.container {margin: 24px auto;padding: 16px;max-width: 720px;}.info {display: flex;align-items: center;}.info>div>span, .info>div>p {font-weight: 400;font-size: 13px;color: #747487;line-height: 16px;}.info>div>span::before {content: "👋";}.info>div>h2 {padding: 8px 0 6px;margin: 0;}.info>div>p {padding: 0;margin: 0;}.info>img {background: #747487;height: 96px;width: 96px;border-radius: 31.68px;overflow: hidden;margin: 0 20px 0 0;}.response {margin: 32px 0;display: flex;flex-wrap: wrap;align-items: center;justify-content: space-between;}.response>a {text-decoration: none;color: #2D8CFF;font-size: 14px;}.response>pre {overflow-x: scroll;background: #f6f7f9;padding: 1.2em 1.4em;border-radius: 10.56px;width: 100%;box-sizing: border-box;}
-                        //     </style>
-                        //     <div class="container">
-                        //         <div class="info">
-                        //             <img src="${body.pic_url}" alt="User photo" />
-                        //             <div>
-                        //                 <span>Hello World!</span>
-                        //                 <h2>${body.first_name} ${body.last_name}</h2>
-                        //                 <p>${body.role_name}, ${body.company}</p>
-                        //             </div>
-                        //         </div>
-                        //         <div class="response">
-                        //             <h4>JSON Response:</h4>
-                        //             <a href="https://marketplace.zoom.us/docs/api-reference/zoom-api/users/user" target="_blank">
-                        //                 API Reference
-                        //             </a>
-                        //             ${JSONResponse}
-                        //         </div>
-                        //     </div>
-                        // `);
-                        res.redirect(NGROK_LINK + "/oauth2callback");
-
-                        authenticate(['https://www.googleapis.com/auth/plus.me'])
-                            .catch(console.error);
+                        authenticate().catch(console.error);
                     }
                 }).auth(null, null, true, body.access_token);
 
@@ -106,13 +79,15 @@ app.get('/', (req, res) => {
         return;
 
     }
-    async function authenticate(scopes) {
+    async function authenticate() {
         return new Promise((resolve, reject) => {
-            // grab the url that will be used for authorization
-            const authorizeUrl = oauth2Client.generateAuthUrl({
+
+            // redirect to the google oauth url
+            res.redirect(oauth2Client.generateAuthUrl({
                 access_type: 'offline',
-                scope: scopes.join(' '),
-            });
+                scope: ['https://www.googleapis.com/auth/plus.me'].join(' '),
+            }));
+
             const server = http
                 .createServer(async (req, res) => {
                     try {
@@ -129,19 +104,9 @@ app.get('/', (req, res) => {
                         reject(e);
                     }
                 })
-                .listen(3000, () => {
-                    // open the browser to the authorize url to start the workflow
-                    opn(authorizeUrl, { wait: false }).then(cp => cp.unref());
-                });
             destroyer(server);
         });
     }
-
-    // async function runSample() {
-    //     // retrieve user profile
-    //     const res = await plus.people.get({ userId: 'me' });
-    //     console.log(res.data);
-    // }
 
     // Step 2: 
     // If no authorization code is available, redirect to Zoom OAuth to authorize
